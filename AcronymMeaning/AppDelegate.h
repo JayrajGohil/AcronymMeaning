8f2bd21dfdc3b380789d8577f9384ed706af8863
Jayraj Gohil <softwaremerchant@softwares-imac.home>
2017-01-25 10:01:41 -0500

    Initial Commit

---
+++ AcronymMeaning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcronymMeaning
+//
+//  Created by Software Merchant on 1/25/17.
+//  Copyright © 2017 Instock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcronymMeaning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcronymMeaning
+//
+//  Created by Software Merchant on 1/25/17.
+//  Copyright © 2017 Instock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
